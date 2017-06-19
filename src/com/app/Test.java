fa3e804dc0fc52505896b5ccb6d26a54bfe00819
Jhanakiram <Jhanakiram@192.168.0.2>
2017-06-19 21:08:21 +0530

    test changes

---
+++ src/com/app/Test.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Employee emp=new Employee();
+		emp.setId(10);
+		emp.setName("AA");
+		emp.setSal(12.32);
+		System.out.println(emp);
+	}
+}
